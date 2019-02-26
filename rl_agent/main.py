327122e02d1857a1c99fd226a602d0d51a536227
Pablo Beltran <pa945249@ucf.edu>
2019-02-26 16:57:49 -0500

    fixed entropy bug

--- rl_agent/main.py
+++ rl_agent/main.py
@@ -58,7 +58,7 @@ if FLAGS.use_tensorboard:
     stamp = date.strftime('%Y.%m.%d_%H.%M')
     title = FLAGS.map + "_" + stamp + "_x" + str(FLAGS.parallel)
     if FLAGS.tensorboard_dir != None:
-      title = title + "_" + tensorboard_dir
+      title = title + "_" + FLAGS.tensorboard_dir
     TBDIR = './tboard/' + title
 
 
@@ -194,7 +194,7 @@ def _main(unused_argv):
     time.sleep(5)
 
   run_thread(agents[-1], FLAGS.map, FLAGS.render, stats)
-
+  #print("HERE WE GFOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
   for t in threads:
     t.join()
   summary_writer.close()
